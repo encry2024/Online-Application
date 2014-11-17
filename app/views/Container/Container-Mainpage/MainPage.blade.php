0ee882b51ea1e2d6f3a10b7c0c23c5b60fd25a8f
Jake <christan.jake2025@yahoo.com>
2014-11-17 08:14:18 +0800

    Minor change

--- app/views/Container/Container-Mainpage/MainPage.blade.php
+++ app/views/Container/Container-Mainpage/MainPage.blade.php
@@ -2,7 +2,7 @@
 
 
 
-<div class="large-10 small-12 columns large-centered">
+<div class="large-8 small-12 columns large-centered">
 	<div class="row">
 		@include('Fields.MainpageFields.mainpagetable')
 	</div>
--- app/views/Sidebars/Sidebar-Mainpage/Sidebar.blade.php
+++ app/views/Sidebars/Sidebar-Mainpage/Sidebar.blade.php
@@ -5,9 +5,9 @@
 	<div class="sidebar">
 		<ul class="side-nav">
 		<br><br><br><br><br>
-			<li>{{ link_to('','New Applicants', ['class'=>'text-center', 'id'=>'', 'name'=>'']) }}</li>
+			<li>{{ link_to('','New Applicants', ['class'=>'text-center large-12', 'id'=>'', 'name'=>'']) }}</li>
 			<li><div class="separator"></div></li>
-			<li>{{ link_to('','Print', ['class'=>'text-center', 'id'=>'', 'name'=>'']) }}</li>
+			<li>{{ link_to('','Print', ['class'=>'text-center large-12', 'id'=>'', 'name'=>'']) }}</li>
 		</ul>
 	</div>
 </div>
